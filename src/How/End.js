f466c4d3a041730e6141c06c5ead7cdbb4e40a92
Xin Yu <66938425+chuahxinyu@users.noreply.github.com>
2022-01-21 10:14:09 +0800

    Updates

--- src/How/End.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function End() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/How/HowTwo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HowTwo() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
